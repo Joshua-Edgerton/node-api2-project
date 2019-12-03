134cff3a800001fffa5f5403738906ac24245745
Joshua Edgerton <joshuaxedgerton@gmail.com>
2019-12-03 15:28:18 -0600

    All GET requests working through router

--- api/server.js
+++ api/server.js
@@ -5,6 +5,36 @@ server.use(express.json());
 const db = require ('../data/db');
 server.use('/api/posts', postsRouter);
 
+server.get('/api/posts', (req, res) => {
+    db.find(req.query)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Error getting posts /api/posts' })
+        });
+});
+
+server.get('/api/posts/:id', (req, res) => {
+    db.findById(req.params.id)
+        .then(postsId => {
+            res.status(200).json(postsId);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Error getting posts IDs /api/posts/:id' })
+        });
+});
+
+server.get('/api/posts/:id/comments', (req, res) => {
+    const id = req.params.id
+    db.findPostComments(id)
+        .then(comments => {
+            res.status(200).json(comments);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Error getting comments /api/posts/:id/comments' });
+        });
+});
 
 
 
--- posts/posts-router.js
+++ posts/posts-router.js
@@ -4,7 +4,36 @@ const router = express.Router();
 router.use(express.json());
 const db = require('../data/db');
 
+router.get('/', (req, res) => {
+    db.find(req.query)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Error getting posts /api/posts' })
+        });
+});
 
+router.get('/:id', (req, res) => {
+    db.findById(req.params.id)
+        .then(postsId => {
+            res.status(200).json(postsId);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Error getting posts IDs /api/posts/:id' })
+        });
+});
+
+router.get('/:id/comments', (req, res) => {
+    const id = req.params.id
+    db.findPostComments(id)
+        .then(comments => {
+            res.status(200).json(comments);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Error getting comments /api/posts/:id/comments' });
+        });
+});
 
 
 
