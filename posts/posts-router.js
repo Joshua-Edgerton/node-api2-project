33fcbd641d099075dfba4570babc2c023ad053e3
Joshua Edgerton <joshuaxedgerton@gmail.com>
2019-12-04 09:15:09 -0600

    fixed PUT errors

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -94,7 +94,14 @@ router.put('/:id', (req, res) => {
     const id = req.params.id;
     db.update(id, changes)
         .then(updated => {
+            console.log(updated)
+            if (updated === 1 && req.body.title && req.body.contents) {
             res.status(200).json({ message: "amount of elements updated", updated });
+            } else if (!req.body.title || !req.body.contents) {
+                res.status(400).json({ message: "Please provide title and contents" })
+            } else if (updated === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" });
+            }
         })
         .catch(error => {
             res.status(500).json({ message: 'Error updating the comment /api/posts/:id', error });
