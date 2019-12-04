b622fff09fb92d318bb1a919e8af8567c810aae2
Joshua Edgerton <joshuaxedgerton@gmail.com>
2019-12-04 09:31:27 -0600

    DELETE error messages fixed

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -82,7 +82,12 @@ router.delete('/:id', (req, res) => {
     const id = req.params.id;
     db.remove(id)
         .then(removed => {
+            console.log(removed)
+            if (removed === 1) {
             res.status(200).json({ message: "Succesfully removed", removed });
+            } else {
+                res.status(404).json({ message: "A post with that ID does not exist"});
+            }
         })
         .catch(error => {
             res.status(500).json({ message: 'Error removing the comment /api/posts/:id', error });
