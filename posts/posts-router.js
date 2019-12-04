853f334d94a2de60b2b276690866ec54155320bf
Joshua Edgerton <joshuaxedgerton@gmail.com>
2019-12-03 18:03:48 -0600

    MVP met updated

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -5,23 +5,35 @@ router.use(express.json());
 const db = require('../data/db');
 
 router.post('/', (req, res) => {
-    db.insert(req.body)
+    if (req.body.title && req.body.contents){
+        db.insert(req.body)
         .then(response => {
-            res.status(200).json({ message: "Succesfully posted with ID", response })
+            res.status(201).json({ message: "Succesfully posted with ID", response })
         })
         .catch(error => {
             res.status(500).json({ message: 'Error posting new data /api/posts', error })
         })
+    } else {
+        res.status(400).json({ message: "Please provide title and contents for post" })
+    }
+
 })
 
 router.post('/:id/comments', (req, res) => {
-    db.insertComment(req.body)
+    
+    if (!req.params.id) {
+        res.status(404).json({ message: "The post with that ID does not exist" })
+    } else if (!req.body.text) {
+        res.status(400).json({ message: "Please provide text" })
+    } else {
+        db.insertComment(req.body)
         .then(response => {
             res.status(200).json({ message: 'Comment posted succesfully', response})
         })
         .catch(error => {
             res.status(500).json({ message: 'Error posting new comment /api/posts/:id/comments', error })
         })
+    }
 })
 
 router.get('/', (req, res) => {
@@ -30,29 +42,36 @@ router.get('/', (req, res) => {
             res.status(200).json(posts);
         })
         .catch(error => {
-            res.status(500).json({ message: 'Error getting posts /api/posts' })
+            res.status(500).json({ message: 'Error getting posts /api/posts', error })
         });
 });
 
 router.get('/:id', (req, res) => {
     db.findById(req.params.id)
         .then(postsId => {
-            res.status(200).json(postsId);
+            postsId ?
+            res.status(200).json(postsId) : 
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
         })
         .catch(error => {
-            res.status(500).json({ message: 'Error getting posts IDs /api/posts/:id' })
-        });
+            res.status(500).json({ message: 'Error getting posts IDs /api/posts/:id', error })
+        })
+
 });
 
 router.get('/:id/comments', (req, res) => {
+    if (!req.params.id) {
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+    } else {
     const id = req.params.id
     db.findPostComments(id)
         .then(comments => {
             res.status(200).json(comments);
         })
         .catch(error => {
-            res.status(500).json({ message: 'Error getting comments /api/posts/:id/comments' });
-        });
+            res.status(500).json({ message: 'Error getting comments /api/posts/:id/comments', error });
+        })
+    }
 });
 
 router.delete('/:id', (req, res) => {
