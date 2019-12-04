614265afec6a11cf5c55b10f273ad6274377c4c4
Joshua Edgerton <joshuaxedgerton@gmail.com>
2019-12-03 18:44:37 -0600

    MVP met

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -46,18 +46,22 @@ router.get('/', (req, res) => {
         });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
     db.findById(req.params.id)
-        .then(postsId => {
-            postsId ?
-            res.status(200).json(postsId) : 
-            res.status(404).json({ message: "The post with the specified ID does not exist." })
-        })
-        .catch(error => {
-            res.status(500).json({ message: 'Error getting posts IDs /api/posts/:id', error })
-        })
-
-});
+      .then(post => {
+        if (post) {
+          res.status(200).json(post);
+        } else {
+          res.status(404).json({ message: "Post not found" });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({
+          message: "Error retrieving the post with the specified id"
+        });
+      });
+  });
 
 router.get('/:id/comments', (req, res) => {
     if (!req.params.id) {
